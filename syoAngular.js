b525927b902a8b0a93af98342552b2ead71133e6
Gustavo Henke <guhenke@gmail.com>
2016-07-14 17:23:22 -0300

    chore(release): versão 0.10.0

--- syoAngular.js
+++ syoAngular.js
@@ -15,6 +15,26 @@
 		"syonet.tooltip"
 	]);
 }( angular );
+!function( ng ) {
+	"use strict";
+
+	ng.module( "syonet" ).filter( "autodate", [ "$filter", function( $filter ) {
+		var date = $filter( "date" );
+
+		return function( input, adjunct ) {
+			var day = date( input, "dd/MM/yyyy" );
+			var hour = date( input, "HH:mm" );
+
+			// Hora zerada não retorna nada
+			if ( hour === "00:00" ) {
+				return day;
+			}
+
+			return day + ( adjunct ? " às" : "" ) + " " + hour;
+		};
+	}]);
+
+}( angular );
 !function( $, ng ) {
 	"use strict";
 
